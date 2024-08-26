92130559f3e447093e65950c4e50c3f095003206
Guillaume <guillaumeargensse@hotmail.fr>
2024-08-26 16:50:12 +0200

    fix a bug causing an error when multiple tag were applied

--- js/utils/dropdown-filter.js
+++ js/utils/dropdown-filter.js
@@ -177,9 +177,9 @@ function filterRecipeWithTag(arrayOfActiveTag) {
 
         let arrayOfFilteredRecipe = [];
         let arrayToFilter = arrayOfRecipeObject;
-        let indexSet = new Set();
-
+        
         for (let i = 0; i < arrayOfActiveTag.length; i++) {
+            let indexSet = new Set();
 
             for (let k = 0; k < arrayToFilter.length; k++) {
 
